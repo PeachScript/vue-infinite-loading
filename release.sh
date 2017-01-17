b2fcda1c15fa5c0ad33d29417c142e80fcf39eb6
Peach <scdzwyxst@gmail.com>
2017-01-17 17:28:38 +0800

    Remove the next tag when publish to npm

--- release.sh
+++ release.sh
@@ -21,7 +21,7 @@ then
   npm version $VERSION -m "Upgrade to $VERSION"
 
   # publish
-  npm publish --tag next
+  npm publish
   echo "Publish $VERSION successfully!"
 
   read -p "Upgrade GitHub Pages? (y/n)" -n 1 -r
