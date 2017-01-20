886c38d7ef62df8361e7761e37405dc6f9636f6f
Peach <scdzwyxst@gmail.com>
2017-01-20 21:24:56 +0800

    Improve documentation for server-side rendering

--- src/components/ServerSideRendering.vue
+++ src/components/ServerSideRendering.vue
@@ -2,15 +2,21 @@
   <h3>Server-Side Rendering</h3>
   <p>Server-Side Rendering(SSR) is a <a target="_blank" href="https://vuejs.org/v2/guide/ssr.html">new official feature</a> in <code>Vue.js 2.0</code>, but we will get an error like this when you use this component in your SRR app:</p>
   <pre v-highlightjs>
-    ReferenceError: window is not defined
+Error: window is not defined
+
+ReferenceError: window is not defined
     at ...
     at ...
-    at ...
-    at ...
-    at ...
-    at ...
+    at e.exports (...)
+    at Object. (...)
+    at p (...)
+    at Object.e.exports.render.e (...)
+    at p (...)
+    at Object. (...)
+    at p (...)
+    at e.__esModule.default (...)
   </pre>
-  <p>Because of the <code>style-loader</code> is not support local export, details in <a target="_blank" href="https://github.com/webpack/style-loader/pull/159">here</a>, so we need this workaround for your SSR app:</p>
+  <p>Because the <code>style-loader</code> does not support local export at this time, details in <a target="_blank" href="https://github.com/webpack/style-loader/pull/159">here</a>, so we need this workaround for your SSR app:</p>
   <ol>
     <li>
       use
