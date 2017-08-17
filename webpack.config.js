9737aca4a8068c3c3341cffc6b6b1bb79db8e005
Peach <scdzwyxst@gmail.com>
2017-08-17 17:24:40 +0800

    Remove useless require

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const webpack = require('webpack');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
