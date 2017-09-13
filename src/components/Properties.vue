5acd051be39eb60eeee940294c127e95d1277c52
Peach <scdzwyxst@gmail.com>
2017-09-13 13:19:03 +0800

    Update properties for the v2.2.x

--- src/components/Properties.vue
+++ src/components/Properties.vue
@@ -1,15 +1,20 @@
 <template>
   <h3>Properties</h3>
 
-  <h4>on-infinite</h4>
-  <p>This is a callback function which will be called when scroll to the specific distance from the bottom of scroll parent.</p>
-  <p>Usually, you should to send the <code>$InfiniteLoading:loaded</code> event in this function after loaded data completely.</p>
-  <pre>
-- type      Function
-- required  true</pre>
+  <div v-show="$parent.docVersion < 2.2">
+    <h4>on-infinite</h4>
+    <p>This is a callback function which will be called when scroll to the specific distance from the bottom of scroll parent.</p>
+    <p>Usually, you should to send the <code>$InfiniteLoading:loaded</code> event in this function after loaded data completely.</p>
+    <pre>
+  - type      Function
+  - required  true</pre>
+  </div>
 
   <h4>distance</h4>
-  <p>This is a critical value for scroll. If the distance from the bottom of scroll parent less than this property, the <code>on-infinite</code> callback will be called.</p>
+  <p>This is a critical value for scroll. If the distance from the bottom of scroll parent less than this property,
+  <span v-show="$parent.docVersion < 2.2">the <code>on-infinite</code> callback will be called.</span>
+  <span v-show="$parent.docVersion >= 2.2">the <code>infinite</code> event will be triggered.</span>
+  </p>
   <pre>
 - type      Number
 - required  false
@@ -35,9 +40,15 @@
 
   <div v-show="$parent.docVersion >= 1.3 && $parent.docVersion < 2 || $parent.docVersion >= 2.01">
     <h4>direction</h4>
-    <p>If you set this propery to <code>top</code>, this componet will call the <code>on-infinite</code> function when you scroll to top.</p>
+    <p>If you set this propery to <code>top</code>,
+    <span v-show="$parent.docVersion < 2.2">this componet will call the <code>on-infinite</code> function</span>
+    <span v-show="$parent.docVersion >= 2.2">this componet will triggere the <code>infinite</code> event</span>
+    when you scroll to top.</p>
     <div class="warning-prompt">
-      Warning: You must set the <code>scrollTop</code> of the scroll parent to correct value <strong>manually</strong> after load data, if not, this component will call the <code>on-infinite</code> function again and again.
+      Warning: You must set the <code>scrollTop</code> of the scroll parent to correct value <strong>manually</strong> after load data, if not,
+      <span v-show="$parent.docVersion < 2.2">this component will call the <code>on-infinite</code> function</span>
+      <span v-show="$parent.docVersion >= 2.2">this component will triggere the <code>infinite</code> event</span>
+      again and again.
     </div>
     <pre>
 - type      String
