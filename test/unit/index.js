b5a5dcf506c4be39afdc9a9a0700272f9807efaf
Peach <scdzwyxst@gmail.com>
2018-08-11 00:19:09 +0800

    Improve unit tests for passive event support logic

--- test/unit/index.js
+++ test/unit/index.js
@@ -1,3 +1,17 @@
+// mock passive event listener support and upsupport
+(() => {
+  const originAddListener = window.addEventListener;
+  let flag;
+
+  window.addEventListener = (...args) => {
+    // try to read passive property and only read once time if it is accessible
+    if (!flag && args[2] && args[2].passive) {
+      flag = false;
+    }
+    originAddListener.apply(window, args);
+  };
+})();
+
 const testsContext = require.context('./specs', true, /\.spec$/);
 testsContext.keys().forEach(testsContext);
 
