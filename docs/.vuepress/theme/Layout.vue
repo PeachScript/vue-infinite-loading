38c68a24ec3efa43839ae9491822c6e94e8e2701
Peach <scdzwyxst@gmail.com>
2018-09-30 18:21:59 +0800

    docs: add basic Layout component through extend way

--- docs/.vuepress/theme/Layout.vue
+++ docs/.vuepress/theme/Layout.vue
@@ -1,190 +1,193 @@
 <template>
-  <div>
-    <header>
-      <div class="container">
-        <router-link :to="{ path: '/' }" id="logo"></router-link>
-        <h1>Vue-infinite-loading</h1>
-      </div>
-    </header>
-    <main class="container">
-      <previewer class="previewer"></previewer>
-      <div class="intro-container">
-        <p>An infinite scroll plugin for Vue.js</p>
-        <button class="button button-large button-basic">Get Started</button>
-        <button class="button button-large">View GitHub</button>
-        <ul class="feat-list">
-          <li>
-            <h3>Out of the box</h3>
-            BalabalaBalabalaBalabalaBalabala
-          </li>
-          <li>
-            <h3>2-direction support</h3>
-            BalabalaBalabalaBalabalaBalabala
-          </li>
-          <li>
-            <h3>Result display</h3>
-            BalabalaBalabalaBalabalaBalabala
-          </li>
-        </ul>
-      </div>
-    </main>
+  <div
+    class="theme-container"
+    :class="[{ 'doc-mode': !isHomepage }, ...pageClasses]"
+    @touchstart="onTouchStart"
+    @touchend="onTouchEnd"
+  >
+    <router-link :to="$localePath" id="logo"></router-link>
+    <h1 v-text="$siteTitle"></h1>
+
+    <Navbar
+      v-if="shouldShowNavbar"
+      @toggle-sidebar="toggleSidebar"
+    />
+
+    <div
+      class="sidebar-mask"
+      @click="toggleSidebar(false)"
+    ></div>
+
+    <Sidebar
+      :items="sidebarItems"
+      @toggle-sidebar="toggleSidebar"
+    >
+      <slot
+        name="sidebar-top"
+        slot="top"
+      />
+      <slot
+        name="sidebar-bottom"
+        slot="bottom"
+      />
+    </Sidebar>
+
+    <div
+      class="custom-layout"
+      v-if="$page.frontmatter.layout"
+    >
+      <component :is="$page.frontmatter.layout"/>
+    </div>
+
+    <Page
+      v-else
+      :sidebar-items="sidebarItems"
+    >
+      <slot
+        name="page-top"
+        slot="top"
+      />
+      <slot
+        name="page-bottom"
+        slot="bottom"
+      />
+    </Page>
+
+    <Previewer/>
+    <Intro/>
+
+    <SWUpdatePopup :updateEvent="swUpdateEvent"/>
+    <footer class="footer">
+      <p>Released under the MIT License</p>
+      <p>&copy;2016-present Made with ♥ under Vuepress by PeachScript</p>
+    </footer>
   </div>
 </template>
 
 <script>
 import 'focus-visible';
-import Previewer from './Previewer';
+import OfficialLayout from 'vuepress/lib/default-theme/Layout';
+import Intro from './components/Intro';
+import Previewer from './components/Previewer';
 
 export default {
+  extends: OfficialLayout,
+  computed: {
+    isHomepage() {
+      return this.$page.frontmatter.home;
+    },
+  },
   components: {
+    Intro,
     Previewer,
-  }
+  },
 };
 </script>
 
-<style lang="less">
-@import './less/button.less';
+<style lang="stylus">
+@require './styles/button';
+@require './styles/config';
 
-body {
-  margin: 0;
-  font: 16px/1.42857 PingFang SC, Lantinghei SC, Microsoft Yahei, Hiragino Sans GB, Microsoft Sans Serif, WenQuanYi Micro Hei, sans-serif;
-  background-color: #f9fbfd;
-}
-</style>
+.theme-container
+  .previewer
+    position absolute
+    z-index 100
+    top $s-header-height + 10
+    right 100% - $s-home-divide-ratio
+    margin-right $s-home-middle-gap
+    transition all 0.6s cubic-bezier(0.645, 0.045, 0.355, 1)
 
-<style lang="less" scoped>
-@import './less/variables.less';
+  #logo
+    position absolute
+    z-index 100
+    top 100px
+    left $s-home-divide-ratio
+    display inline-block
+    margin-left 216px + $s-home-middle-gap
+    width 200px
+    height 200px
+    background url('./assets/images/logo.png') no-repeat center/100%
 
-.container {
-  margin: 0 auto;
-  width: 1200px;
-}
+    + h1
+      position absolute
+      z-index 100
+      top 280px
+      left $s-home-divide-ratio
+      margin-left 164px + $s-home-middle-gap
+      color $c-basic
+      font-size 32px
 
-header .container,
-main.container {
-  position: relative;
-}
+    &,
+    + h1
+      transition all 0.6s cubic-bezier(0.645, 0.045, 0.355, 1)
 
-#logo {
-  position: absolute;
-  z-index: 10;
-  top: 10px;
-  left: 50%;
-  margin-left: -600px;
-  display: inline-block;
-  width: 60px;
-  height: 60px;
-  background: url('./assets/images/logo.png') no-repeat center/100%;
+  .navbar
+    transition all 0.3s
 
-  + h1 {
-    position: absolute;
-    top: 10px;
-    left: 50%;
-    margin: 12px 0 0 -520px;
-    z-index: 10;
-    color: @c-basic;
-    font-size: 24px;
+    .home-link
+      display none
 
-  }
+    .links
+      transition all 0.3s
 
-  &,
-  + h1 {
-    transition: all 1s cubic-bezier(0.23, 1, 0.32, 1);
-  }
+  .intro-container
+    position absolute
+    z-index 10
+    top 346px
+    left $s-home-divide-ratio
+    margin-left $s-home-middle-gap
+    transition all 0.3s
+    transition-delay 0.6s
 
-  &.router-link-active {
-    top: 100px;
-    left: 38%;
-    margin-left: 236px;
-    width: 200px;
-    height: 200px;
+  .footer
+    margin-top 700px
+    padding 15px 0
+    text-align center
+    background-color #fff
+    transition all 0.3s
 
-    + h1 {
-      top: 280px;
-      left: 38%;
-      margin-left: 179px;
-      font-size: 32px;
-    }
-  }
-}
+    p
+      margin 0
+      color lighten($c-basic-light, 15%)
+      font-size 14px
 
-.previewer {
-  position: absolute;
-  z-index: 10;
-  top: 80px;
-  right: 62%;
-  margin-right: 20px;
-}
+  &:not(.doc-mode)
+    .navbar
+      background transparent
 
-.intro-container {
-  position: absolute;
-  z-index: 10;
-  left: 38%;
-  margin-top: 338px;
-  margin-left: 20px;
-  width: 640px;
-  text-align: center;
+      .links
+        opacity 0
+        visibility hidden
+        transform translateY(10px)
+  &.doc-mode
+    #logo
+      top 5px
+      left $s-edge-gap
+      margin-left 0
+      width 50px
+      height 50px
+      transition-delay 0.3s
 
-  > p {
-    margin: 0 0 40px;
-    color: @c-basic-light;
-  }
+      + h1
+        top 2px
+        left $s-edge-gap + 60
+        margin 12px 0 0
+        font-size 24px
+        transition-delay 0.3s
 
-  .button + .button {
-    margin-left: 20px;
-  }
+    .previewer
+      position fixed
+      right $s-edge-gap
+      margin-right 0
+      transition-delay 0.3s
 
-  .feat-list {
-    list-style: none;
-    display: flex;
-    margin-top: 60px;
-    padding: 0;
+    .intro-container
+      opacity 0
+      visibility hidden
+      transform translateY(30px)
+      transition-delay 0s
 
-    li {
-      position: relative;
-      padding-top: 120px;
-      flex: 1;
-      color: @c-basic-light;
-      word-break: break-all;
-
-      &:not(:last-child) {
-        margin-right: 20px;
-      }
-
-      &::before {
-        content: '';
-        position: absolute;
-        z-index: 1;
-        top: 0;
-        left: 0;
-        width: 100%;
-        padding-top: 80%;
-        background: no-repeat center top/90%;
-        filter: grayscale(50%);
-        transition: filter 0.3s;
-      }
-
-      &:first-child::before {
-        background-image: url('./assets/images/icon-box.png');
-      }
-
-      &:nth-child(2)::before {
-        background-image: url('./assets/images/icon-dir.png');
-      }
-
-      &:last-child::before {
-        background-image: url('./assets/images/icon-msg.png');
-      }
-
-      &:hover::before {
-        filter: none;
-      }
-
-      h3 {
-        margin: 0 0 5px;
-        color: @c-basic;
-      }
-    }
-  }
-}
+    .footer
+      opacity 0
+      visibility hidden
 </style>
